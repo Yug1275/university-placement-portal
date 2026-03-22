e96e86d5ed7fdb8af3a2144eb968495743d351b7
Yug1275 <yjpatel1275@gmail.com>
2026-03-22 19:58:42 +0530

    Phase 3: Implement authentication with JWT, bcrypt, and protected routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
+const authRoutes = require("./routes/authRoutes");
+
 
 dotenv.config();
 
@@ -12,6 +14,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 // Test route
 app.get("/", (req, res) => {
